ad6c6f5bac0522472b141d555930936aa6351f67
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-06-28 09:44:11 -0700

    Create telegrammiss.py

---
+++ telegrammiss.py
@@ -0,0 +1,11 @@
+from userbot PYTHON await 
+from missrose_bot (int.run)
+from userbot HACK.
+                   hack=PRO.int(LEGENDHACKER)
+                       PRO=int(@missrose_bot(str(.77690987675-pro)
+                  hack=-user-id(None,)
+                       user=id-in , if ,None ,await:PRO()
+                  hack userbot(cmd)-run ,pro
+                              mirror_mod_on==X
+                              mirror_mod_off=Y
+                                             

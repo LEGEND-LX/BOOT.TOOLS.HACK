0a87ea2791b4d1dac66a5efb818ec8dbc0267ee6
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-06-28 09:48:59 -0700

    Create pro.js

---
+++ pro.js
@@ -0,0 +1,9 @@
+FROM Run await:pro
+         hacker==PRO+LEGEND=HACKER
+                            LODING_MODE=int("LEGENDHACKER_HACK").SEND
+                            run script(int.str("HACKL")
+                                                       RUN()loding......
+                            Run_Pro(JAVA"PYTHON"LINUX await "HACK")).send_messege.
+                            run userbot file if not pro=legendbot("@missrose_bot".miss))
+                                                         protection=CYBER.SELS.adding<ROT>
+                                                                   =CYBER=DARKWEB=TELETHON......
